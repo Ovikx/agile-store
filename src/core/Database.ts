178d1eafa41eacd572db7836d33ca8010af1ef04
Ovikx <ovikdas10@gmail.com>
2023-07-17 13:58:54 -0400

    chore: create base db object

--- src/core/Database.ts
+++ src/core/Database.ts
@@ -1,1 +1,25 @@
-export class Database {}
+export async function createDB(
+  dbName: string,
+  version?: number,
+): Promise<Database> {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open(dbName, version);
+
+    request.addEventListener("success", () => {
+      console.log(`Opened DB instance ${dbName}`);
+      resolve(new Database(request.result));
+    });
+
+    request.addEventListener("error", () => {
+      console.log(request.error);
+      reject(request.error);
+    });
+  });
+}
+
+export class Database {
+  _db: IDBDatabase;
+  constructor(db: IDBDatabase) {
+    this._db = db;
+  }
+}
