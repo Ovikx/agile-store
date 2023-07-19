88b43792fc999c4618335cb4ac1ad23378022ea1
Ovikx <ovikdas10@gmail.com>
2023-07-18 20:37:35 -0400

    feat: add sample store to example app

---
+++ example-vite-react/src/db/db.ts
@@ -0,0 +1,7 @@
+import { Store } from "agile-store";
+import { Item } from "../types/types";
+export const itemsStore = new Store<Item>({
+  name: "items",
+  keyPath: "name",
+  autoIncrement: false,
+});
--- example-vite-react/src/main.tsx
+++ example-vite-react/src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { createStores } from "agile-store";
+import { itemsStore } from "./db/db.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+createStores("test-db", 1, [itemsStore])
+  .then(() => {
+    ReactDOM.createRoot(document.getElementById("root")!).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  })
+  .catch((err) => console.log(err));
